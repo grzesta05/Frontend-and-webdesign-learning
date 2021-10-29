e6002e1a07adacaacd071516b7f2787d6a123526
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2021-10-29 12:33:21 +0000

    Dziedziczenie 29.10
    Zadanie na aint

---
+++ Ćwiczenia aint/Majgier Grzegorz - Wyzwanie - Dziedziczenie/script.js
@@ -0,0 +1,19 @@
+console.log("Daję ci okejkę");
+const a0 = ["1", "3", "1", "4", "0"];
+const a1 = ["1", "0", "2", "0", "2"];
+const a2 = ["1", "0", "3", "0", "2"];
+function functionYN()
+{
+    document.getElementById("solution").style.display = "none";
+    document.getElementById("nie").style.display = "inline";
+    document.getElementById("tak").style.display = "inline";
+
+
+}
+
+function myGuesses()
+{
+    var red0 = document.getElementsByClassName("red0");
+    var red1 = document.getElementsByClassName("red1");
+    var red2 = document.getElementsByClassName("red2");
+}
