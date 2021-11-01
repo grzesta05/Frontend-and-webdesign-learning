5e923561591f663339ba6427c4da85ee4f8b064c
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2021-11-01 12:24:57 +0100

    Added working script
    Smooth image transition in progress

--- Ćwiczenia aint/Majgier Grzegorz - Wyzwanie - Dziedziczenie/script.js
+++ Ćwiczenia aint/Majgier Grzegorz - Wyzwanie - Dziedziczenie/script.js
@@ -2,18 +2,24 @@ console.log("Daję ci okejkę");
 const a0 = ["1", "3", "1", "4", "0"];
 const a1 = ["1", "0", "2", "0", "2"];
 const a2 = ["1", "0", "3", "0", "2"];
+const a = [a0,a1,a2];
 function functionYN()
 {
     document.getElementById("solution").style.display = "none";
     document.getElementById("nie").style.display = "inline";
     document.getElementById("tak").style.display = "inline";
-
-
 }
 
 function myGuesses()
 {
-    var red0 = document.getElementsByClassName("red0");
-    var red1 = document.getElementsByClassName("red1");
-    var red2 = document.getElementsByClassName("red2");
+  const tab = document.querySelectorAll(".red");
+  for(let i = 0; i <= 2; i++)
+  {
+    const thtab = tab[i].querySelectorAll("th");
+    for(let o = 0; o < thtab.length; o++)
+    {
+      thtab[o].innerHTML = a[i][o];
+      thtab[o].style.color = "red";
+    }
+  }
 }
