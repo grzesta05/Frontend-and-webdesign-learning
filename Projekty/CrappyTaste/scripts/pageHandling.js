e110ba3e7ad9fac24713e3b2e96295603cf13d24
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2021-11-09 17:18:49 +0100

    Working on song list

--- Projekty/CrappyTaste/scripts/pageHandling.js
+++ Projekty/CrappyTaste/scripts/pageHandling.js
@@ -1,2 +1,3 @@
 const audioplayer = document.getElementsByTagName("audio")[0];
 audioplayer.volume = 0.1;
+document.documentElement.style.setProperty('--var', `rgb(${(Math.random()*155)+100},${(Math.random()*155)+100},${(Math.random()*155)+100})`);
---
+++ Projekty/CrappyTaste/scripts/songinfo.js
@@ -0,0 +1,15 @@
+const songs = [
+    {
+        name : "Kolońska i szlugi",
+        author : "Sanah",
+        image : "https://i.ytimg.com/vi/eL-Wyc08Ktc/maxresdefault.jpg",
+        path : `music/sanah - kolońska i szlugi.mp3`
+    },
+    {
+        name : "",
+        author : "",
+        image : "",
+    }
+];
+
+audioplayer.src = songs[0].path;
