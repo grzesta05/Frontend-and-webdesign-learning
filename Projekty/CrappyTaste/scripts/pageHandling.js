5783915abcd237533b5fce21961ce7d5c2bd52ae
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2021-11-09 23:21:01 +0100

    Added new features
    Added:
    - New songs,
    - Song list,
    - Next and previous buttons
    - Random song

--- Projekty/CrappyTaste/scripts/pageHandling.js
+++ Projekty/CrappyTaste/scripts/pageHandling.js
@@ -6,11 +6,46 @@ const songInfo = document.getElementById("songInfo");
 audioplayer.volume = 0.1;
 const randomSongId = Math.floor(Math.random()*songs.length);
 const randomSong = songs[randomSongId];
+document.getElementsByClassName("randomsong")[0].innerHTML =
+ `<button style="width:50%;margin:0;" onclick="playSong(${randomSongId})">
+ <img style = "width:100%;margin:1%;border-radius:10px" 
+ src = ${(songs[randomSongId].image == ""? `https://placekitten.com/${Math.round(Math.random()*1000+500)}/${Math.round(Math.random()*500+400)}` : songs[randomSongId].image)}></button>
+ <p>${randomSong.name} - ${randomSong.author}</p>`
 
-document.getElementsByClassName("randomsong")[0].innerHTML = `<button style="width:50%;margin:0;" onclick="playSong(${randomSongId})"><img style = "width:100%;margin:1%;border-radius:100px" src = ${randomSong.image}></button><p>${randomSong.name} - ${randomSong.author}</p>`
+var currentlyPlaying = 0;
+
+ function renderSongs()
+ {
+    var a = `<div class="row">` ;
+    var x =document.getElementById("SongList");
+    
+    for(let i = 0; i < songs.length; i++)
+    {
+        const img = (songs[i].image == ""? `https://placekitten.com/${Math.round(Math.random()*1000+500)}/${Math.round(Math.random()*400+400)}` : songs[i].image); 
+        a+=`<div class = "col-lg">
+        <button onclick="playSong(${i})" class="songimgbutton" style="background-image: url(${img});"></button>
+        <button class = "p" onclick="playSong(${i})">${songs[i].name}<br />${songs[i].author}</button>
+        </div>`;
+        if((i+1)%3==0)
+        a+=`</div><div class="row">`
+       
+    }
+    a+="</div>";
+    x.innerHTML+=a;
+    console.log(x);
+ }
+ 
+ 
 
 function playSong(id)
 {
     audioplayer.src = songs[id].path;
     songInfo.innerHTML = `${songs[id].name} - ${songs[id].author}`;
+    currentlyPlaying = id;
 }
+function nextOrPrevious(n)
+{
+    playSong(currentlyPlaying+n);
+}
+
+renderSongs();
--- Projekty/CrappyTaste/scripts/songinfo.js
+++ Projekty/CrappyTaste/scripts/songinfo.js
@@ -7,10 +7,48 @@ const songs = [
     },
     {
         name : "You broke me first",
-        author : "Conor Maynard (Tate McRae)",
+        author : "Conor Maynard",
         image : "https://i.ytimg.com/vi/7ecu7DJwgYA/maxresdefault.jpg",
         path : "music/Tate McRae - you broke me first.mp3"
-    }
+    },
+    {
+        name : "Washing Machine Heart",
+        author : "Mitski",
+        image : "https://www.studiodaily.com/wp-content/uploads/2018/11/mitski.jpg",
+        path : "music/Mitski - Washing Machine Heart.mp3"
+    },
+    {
+        name : "Halley's Comet",
+        author : "Billie Eilish",
+        image : "https://www.nme.com/wp-content/uploads/2021/07/Billie-Eilish-Happier-Than-Ever.jpg",
+        path : "music/Billie Eilish - Halley’s Comet.mp3"
+    },
+    {
+        name : "Happier than Ever",
+        author : "Billie Eilish",
+        image : "https://gfx.radiozet.pl/var/radiozetsg/storage/images/muzyka/billie-eilish-wydala-nowa-plyte-happier-than-ever-19-latka-jest-szczesliwsza-niz-kiedykolwiek/16872553-1-pol-PL/Billie-Eilish-szczesliwsza-niz-kiedykolwiek-19-latka-wydala-nowa-plyte-Happier-Than-Ever_article.jpg",
+        path : "music/Billie Eilish - Happier Than Ever.mp3"
+    },
+    {
+        name : "Getting Older",
+        author : "Billie Eilish",
+        image : "https://i.insider.com/612d19249ef1e50018f8c1c9?width=1136&format=jpeg",
+        path : "music/Billie Eilish - Getting Older.mp3",
+    },
+    {
+        name : "Coffee",
+        author : "Beabadoobee",
+        image : "https://lastfm.freetls.fastly.net/i/u/500x500/866221547d0d08c43d20b33e0d6281cb.jpg",
+        path : "music/Beabadoobee - Coffee.mp3",
+    },
+    {
+        name : "",
+        author : "",
+        image : "",
+        path : "",
+    },
 ];
+
+
 
 
