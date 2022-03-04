0a5807d42ecaf45bc9985fcc67308b03b153875a
Grek <43935281+grzesta05@users.noreply.github.com>
2022-03-04 14:34:39 +0100

    bruh part 2

---
+++ Projekt RWD2/js/stylehandling.js
@@ -0,0 +1,77 @@
+
+    s1();
+    function s1()
+    {
+    document.documentElement.style
+    .setProperty("--bg-color","#072227");
+    document.documentElement.style
+    .setProperty("--box-color","#35858B");
+    document.documentElement.style
+    .setProperty("--content-color","#4FBDBA");
+    document.documentElement.style
+    .setProperty("--global-text-color","seashell");
+    document.documentElement.style
+    .setProperty("--global-border-color","lightgrey");
+    
+    document.documentElement.style
+    .setProperty("--link-color","seashell");
+    document.documentElement.style
+    .setProperty("--link-hover-color","rgb(122, 255, 204)");
+    
+   
+    document.documentElement.style
+    .setProperty("--info-color1","#325288");
+    document.documentElement.style
+    .setProperty("--info-color2","#35858B"); 
+    }
+
+    function s2()
+    {
+    document.documentElement.style
+    .setProperty("--bg-color","#AD8B73");
+    document.documentElement.style
+    .setProperty("--box-color","#E3CAA5");
+    document.documentElement.style
+    .setProperty("--content-color","#CEAB93");
+    document.documentElement.style
+    .setProperty("--global-text-color","seashell");
+    document.documentElement.style
+    .setProperty("--global-border-color","#FFFBE9");
+    
+    document.documentElement.style
+    .setProperty("--link-color","seashell");
+    document.documentElement.style
+    .setProperty("--link-hover-color","#7882A4");
+    
+   
+    document.documentElement.style
+    .setProperty("--info-color1","#BB6464");
+    document.documentElement.style
+    .setProperty("--info-color2","#7882A4"); 
+    }
+
+
+    function s3()
+    {
+    document.documentElement.style
+    .setProperty("--bg-color","#f8f9fa");
+    document.documentElement.style
+    .setProperty("--box-color","#edf2f4");
+    document.documentElement.style
+    .setProperty("--content-color","#edf2f4");
+    document.documentElement.style
+    .setProperty("--global-text-color","#2b2d42");
+    document.documentElement.style
+    .setProperty("--global-border-color","#edf2f4");
+    
+    document.documentElement.style
+    .setProperty("--link-color","#2b2d42");
+    document.documentElement.style
+    .setProperty("--link-hover-color","#d90429");
+    
+   
+    document.documentElement.style
+    .setProperty("--info-color1","#8d99ae");
+    document.documentElement.style
+    .setProperty("--info-color2","#e5e5e5"); 
+    }
