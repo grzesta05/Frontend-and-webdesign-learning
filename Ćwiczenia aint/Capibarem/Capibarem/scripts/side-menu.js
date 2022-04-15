051d1720bea1c8fa1f2cc25fa62d033d8e905b03
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2022-04-15 15:34:58 +0200

    finish kapibary

--- Ćwiczenia aint/Capibarem/Capibarem/scripts/side-menu.js
+++ Ćwiczenia aint/Capibarem/Capibarem/scripts/side-menu.js
@@ -3,17 +3,17 @@ const capi = document.querySelector(".side--menu--icon");
 for(let a = 0; a < menu.length; a++)
 {
     menu[a].addEventListener("click", ()=>{
-        document.querySelector(".side--menu--inner").classList.toggle("expand")
+        document.querySelector(".side--menu--inner").style.transform = "scaleY(0)";
         for(let i = 0; i < menu.length; i++)
         {
-            menu[i].classList.remove("moved");
+            menu[i].style.transform = "translateX(0)";
             
         }
         capi.style.top = `${3 * a}rem`;
-        menu[a].classList.toggle("moved");
+        menu[a].style.transform = "translateX(3.5rem)";
         if(document.querySelectorAll(`.side--menu--headers>*:nth-child(${a})`).length != 0)
         {
-            document.querySelector(".side--menu--inner").classList.add("expand")
+            document.querySelector(".side--menu--inner").style.transform = "scaleY(1)";
             console.log("a")
         }
     });
